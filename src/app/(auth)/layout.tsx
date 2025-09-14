515a3791837efffa0a0e84dc7290e7c1fa370016
Sylvester009 <sasy260902@gmail.com>
2025-09-14 20:13:28 +0100

    App sidebar completed2

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,17 +1,7 @@
 import {ThemeProvider} from '@/styles/components/theme-provider';
 import type {Metadata} from 'next';
-import {Geist, Geist_Mono} from 'next/font/google';
 import '../globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'WorkNest',
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,19 +1,8 @@
 import {ThemeProvider} from '@/styles/components/theme-provider';
 import type {Metadata} from 'next';
-import {Geist, Geist_Mono} from 'next/font/google';
 import {AppSidebar} from '@/styles/components/app-sidebar';
 import {SidebarProvider} from '@/styles/components/ui/sidebar';
 import '../globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'WorkNest',
